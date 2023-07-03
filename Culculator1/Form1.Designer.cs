50f86098f7dfe75fa9df8c26e850e2e2ec8b8a7e
densanteacher <mtttakuty@gmail.com>
2023-07-03 12:08:16 +0900

    ラムダ式のテスト

--- Culculator1/Form1.Designer.cs
+++ Culculator1/Form1.Designer.cs
@@ -37,22 +37,22 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
+            this.Rev = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
-            this.button19 = new System.Windows.Forms.Button();
-            this.button20 = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button21 = new System.Windows.Forms.Button();
-            this.button22 = new System.Windows.Forms.Button();
-            this.button23 = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Sq = new System.Windows.Forms.Button();
+            this.Sqrt = new System.Windows.Forms.Button();
             this.button24 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -155,27 +155,26 @@
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.btnNum_Click);
             // 
-            // button10
+            // Rev
             // 
-            this.button10.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button10.Location = new System.Drawing.Point(74, 543);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(80, 80);
-            this.button10.TabIndex = 11;
-            this.button10.Text = "+/-";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.btnReverse_Click);
+            this.Rev.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Rev.Location = new System.Drawing.Point(74, 543);
+            this.Rev.Name = "Rev";
+            this.Rev.Size = new System.Drawing.Size(80, 80);
+            this.Rev.TabIndex = 11;
+            this.Rev.Text = "+/-";
+            this.Rev.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // Dot
             // 
-            this.button11.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button11.Location = new System.Drawing.Point(246, 543);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(80, 80);
-            this.button11.TabIndex = 10;
-            this.button11.Text = ".";
-            this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.btnDot_Click);
+            this.Dot.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Dot.Location = new System.Drawing.Point(246, 543);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(80, 80);
+            this.Dot.TabIndex = 10;
+            this.Dot.Text = ".";
+            this.Dot.UseVisualStyleBackColor = true;
+            this.Dot.Click += new System.EventHandler(this.btnDot_Click);
             // 
             // button12
             // 
@@ -242,38 +241,36 @@
             this.button17.Text = "％";
             this.button17.UseVisualStyleBackColor = true;
             // 
-            // button18
+            // CE
             // 
-            this.button18.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button18.Location = new System.Drawing.Point(160, 113);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(80, 80);
-            this.button18.TabIndex = 18;
-            this.button18.Text = "CE";
-            this.button18.UseVisualStyleBackColor = true;
-            this.button18.Click += new System.EventHandler(this.btnCE_Click);
+            this.CE.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CE.Location = new System.Drawing.Point(160, 113);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(80, 80);
+            this.CE.TabIndex = 18;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
             // 
-            // button19
+            // C
             // 
-            this.button19.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button19.Location = new System.Drawing.Point(246, 113);
-            this.button19.Name = "button19";
-            this.button19.Size = new System.Drawing.Size(80, 80);
-            this.button19.TabIndex = 17;
-            this.button19.Text = "C";
-            this.button19.UseVisualStyleBackColor = true;
-            this.button19.Click += new System.EventHandler(this.btnClear_Click);
+            this.C.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.C.Location = new System.Drawing.Point(246, 113);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(80, 80);
+            this.C.TabIndex = 17;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
             // 
-            // button20
+            // Back
             // 
-            this.button20.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button20.Location = new System.Drawing.Point(332, 113);
-            this.button20.Name = "button20";
-            this.button20.Size = new System.Drawing.Size(80, 80);
-            this.button20.TabIndex = 16;
-            this.button20.Text = "Back";
-            this.button20.UseVisualStyleBackColor = true;
-            this.button20.Click += new System.EventHandler(this.btnBack_Click);
+            this.Back.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Back.Location = new System.Drawing.Point(332, 113);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(80, 80);
+            this.Back.TabIndex = 16;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // textBox1
             // 
@@ -285,43 +282,40 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(312, 40);
+            this.textBox2.Location = new System.Drawing.Point(160, 40);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.Size = new System.Drawing.Size(252, 19);
             this.textBox2.TabIndex = 21;
             // 
-            // button21
+            // Div
             // 
-            this.button21.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button21.Location = new System.Drawing.Point(74, 199);
-            this.button21.Name = "button21";
-            this.button21.Size = new System.Drawing.Size(80, 80);
-            this.button21.TabIndex = 24;
-            this.button21.Text = "1/x";
-            this.button21.UseVisualStyleBackColor = true;
-            this.button21.Click += new System.EventHandler(this.btnDiv_Click);
+            this.Div.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Div.Location = new System.Drawing.Point(74, 199);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(80, 80);
+            this.Div.TabIndex = 24;
+            this.Div.Text = "1/x";
+            this.Div.UseVisualStyleBackColor = true;
             // 
-            // button22
+            // Sq
             // 
-            this.button22.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button22.Location = new System.Drawing.Point(160, 199);
-            this.button22.Name = "button22";
-            this.button22.Size = new System.Drawing.Size(80, 80);
-            this.button22.TabIndex = 23;
-            this.button22.Text = "x^2";
-            this.button22.UseVisualStyleBackColor = true;
-            this.button22.Click += new System.EventHandler(this.btnSq_Click);
+            this.Sq.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sq.Location = new System.Drawing.Point(160, 199);
+            this.Sq.Name = "Sq";
+            this.Sq.Size = new System.Drawing.Size(80, 80);
+            this.Sq.TabIndex = 23;
+            this.Sq.Text = "x^2";
+            this.Sq.UseVisualStyleBackColor = true;
             // 
-            // button23
+            // Sqrt
             // 
-            this.button23.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button23.Location = new System.Drawing.Point(246, 199);
-            this.button23.Name = "button23";
-            this.button23.Size = new System.Drawing.Size(80, 80);
-            this.button23.TabIndex = 22;
-            this.button23.Text = "√x";
-            this.button23.UseVisualStyleBackColor = true;
-            this.button23.Click += new System.EventHandler(this.btnSqrt_Click);
+            this.Sqrt.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Sqrt.Location = new System.Drawing.Point(246, 199);
+            this.Sqrt.Name = "Sqrt";
+            this.Sqrt.Size = new System.Drawing.Size(80, 80);
+            this.Sqrt.TabIndex = 22;
+            this.Sqrt.Text = "√x";
+            this.Sqrt.UseVisualStyleBackColor = true;
             // 
             // button24
             // 
@@ -340,21 +334,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 629);
             this.Controls.Add(this.button24);
-            this.Controls.Add(this.button21);
-            this.Controls.Add(this.button22);
-            this.Controls.Add(this.button23);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.Sq);
+            this.Controls.Add(this.Sqrt);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button17);
-            this.Controls.Add(this.button18);
-            this.Controls.Add(this.button19);
-            this.Controls.Add(this.button20);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.Back);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.button14);
             this.Controls.Add(this.button15);
             this.Controls.Add(this.button16);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button11);
+            this.Controls.Add(this.Rev);
+            this.Controls.Add(this.Dot);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button8);
@@ -383,22 +377,22 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button Rev;
+        private System.Windows.Forms.Button Dot;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.Button button13;
         private System.Windows.Forms.Button button14;
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button button18;
-        private System.Windows.Forms.Button button19;
-        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button Back;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button21;
-        private System.Windows.Forms.Button button22;
-        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Sq;
+        private System.Windows.Forms.Button Sqrt;
         private System.Windows.Forms.Button button24;
     }
 }
--- Culculator1/Form1.cs
+++ Culculator1/Form1.cs
@@ -17,7 +17,50 @@ namespace Culculator1
         {
             InitializeComponent();
             textBox1.Text = "0";
+
+            //クリア（textBox1とtextBox2両方消す）ボタン
+            this.C.Click += (s, e) =>
+            {
+                textBox1.Text = "0";
+                textBox2.Text = null;
+            };
+
+            //CE（textBox1のみ消す）ボタン
+            this.CE.Click += (s, e) => { textBox1.Text = "0"; };
+
+            //+/-(プラスマイナス反転）ボタン
+            this.Rev.Click += (s, e) =>
+            {
+                string txt1 = textBox1.Text;
+                decimal inputRev = -decimal.Parse(txt1);
+                textBox1.Text = inputRev.ToString();
+            };
+
+            //x^2(二乗)ボタン
+            this.Sq.Click += (s, e) =>
+            {
+                string txt1 = textBox1.Text;
+                decimal inputSq = decimal.Parse(txt1) * decimal.Parse(txt1);
+                textBox1.Text = inputSq.ToString();
+            };
+
+            //√x（平方根）ボタン
+            this.Sqrt.Click += (s, e) => {
+                string txt1 = textBox1.Text;
+                double inputSqrt = Math.Sqrt(double.Parse(txt1));
+                textBox1.Text = inputSqrt.ToString();
+            };
+
+            //1/xボタン
+            this.Div.Click += (s, e) =>
+            {
+                string txt1 = textBox1.Text;
+                decimal inputDiv = 1 / decimal.Parse(txt1);
+                textBox1.Text = inputDiv.ToString();
+            };
         }
+        
+        //数字ボタン
         private void btnNum_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
@@ -25,29 +68,34 @@ namespace Culculator1
 
             textBox1.Text = inputNum.ToString();
         }
+
+        //.(小数点）ボタン
         private void btnDot_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
             if (!textBox1.Text.Contains(".")){
-                String inputDot = textBox1.Text + btn.Text;
+                string inputDot = textBox1.Text + btn.Text;
                 textBox1.Text = inputDot.ToString();
             }
         }
+
+        //四則演算ボタン
         private void btnOpe_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
             if((textBox2.Text != null) && (textBox2.Text.Trim().Length != 0))
-            {
-                btnEq_Click(sender, new EventArgs());
-            }
-            String inputOpe = textBox2.Text + textBox1.Text + btn.Text;
+                btnEq_Click(sender,
+                            new EventArgs());
+            string inputOpe = textBox2.Text + textBox1.Text + btn.Text;
             textBox2.Text = inputOpe.ToString();
             textBox1.Text = "0";
         }
+
+        //＝（イコール）ボタン
         private void btnEq_Click(object sender, EventArgs e)
         {
-            String txt2 = textBox2.Text;
-            String mem = txt2.Remove(txt2.Length - 1);
+            string txt2 = textBox2.Text;
+            string mem = txt2.Remove(txt2.Length - 1);
             decimal inputEq = 0;
             if (txt2.Contains("÷"))
                 inputEq = decimal.Parse(mem) / decimal.Parse(textBox1.Text);
@@ -60,47 +108,16 @@ namespace Culculator1
             textBox1.Text = inputEq.ToString();
             textBox2.Text = null;
         }
+
+        //Back(一文字消す）ボタン
         private void btnBack_Click(object sender, EventArgs e)
         {
-            String txt1 = textBox1.Text;
-            String mem = txt1.Remove(txt1.Length - 1);
+            string txt1 = textBox1.Text;
+            string mem = txt1.Remove(txt1.Length - 1);
             if (mem.Length == 0)
                 textBox1.Text = "0";
             else
                 textBox1.Text = mem;
-        }
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "0";
-            textBox2.Text = null;
-        }
-        private void btnCE_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "0";
-        }
-        private void btnReverse_Click(object sender, EventArgs e)
-        {
-            String txt1 = textBox1.Text;
-            decimal inputRev = -decimal.Parse(txt1);
-            textBox1.Text = inputRev.ToString();
-        }
-        private void btnSq_Click(object sender, EventArgs e)
-        {
-            String txt1 = textBox1.Text;
-            decimal inputSq = decimal.Parse(txt1) * decimal.Parse(txt1);
-            textBox1.Text = inputSq.ToString();
-        }
-        private void btnSqrt_Click(object sender, EventArgs e)
-        {
-            String txt1 = textBox1.Text;
-            double inputSqrt = Math.Sqrt(double.Parse(txt1));
-            textBox1.Text = inputSqrt.ToString();
-        }
-        private void btnDiv_Click(object sender, EventArgs e)
-        {
-            String txt1 = textBox1.Text;
-            decimal inputDiv = 1 / decimal.Parse(txt1);
-            textBox1.Text = inputDiv.ToString();
         }
     }
 }
